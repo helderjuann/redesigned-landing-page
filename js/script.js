6af5045e71be1ad33ba3905574b6b0307342a1e0
Heldinnn <helderjuango@gmail.com>
2023-07-24 19:48:54 -0300

    menu-mobile

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    menuMobile();
+
+    function menuMobile() {
+        $('body').on('click','.mobile-menu', function(e) {
+            e.stopPropagation();
+            $(this).find('ul').slideToggle();
+        })
+
+        $('body').on('click', function() {
+            $('body').find('ul').slideUp('open');
+        })
+    }
+})
