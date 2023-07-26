097d493146534ba6bc5dc5aa4c2ce7c9be76d26d
Heldinnn <helderjuango@gmail.com>
2023-07-26 19:19:33 -0300

    Minor Update

--- js/script.js
+++ js/script.js
@@ -13,4 +13,46 @@ $(function() {
             $('.mobile-menu').find('ul').slideUp('open');
         })
     }
-})
+
+    var indexNow = 0;
+    var indexMax = $('.call-2-slider h2').length;
+    var delay = 3000;
+    textSlider();
+
+    function textSlider() {
+        $('.call-2-slider h2').eq(0).fadeIn();
+        setInterval(function() {
+            switchSlider();
+        },delay);
+    }
+
+    function switchSlider() {
+        $('.call-2-slider h2').eq(indexNow).fadeOut(1500);
+        indexNow+= 1;
+        if (indexNow == indexMax)
+            indexNow = 0;
+        $('.call-2-slider h2').eq(indexNow).fadeIn(1500);
+    }
+
+    desktopScroll();
+
+    function desktopScroll() {
+        $('#contact').click(function() {
+            $('html, body').animate({
+                scrollTop: $(document).height()
+            }, 1000);
+            return false;
+        });
+    }
+
+    mobileScroll();
+
+    function mobileScroll() {
+        $('#mobile-contact').click(function() {
+            $('html, body').animate({
+                scrollTop: $(document).height()
+            }, 1000);
+            return false;
+        });
+    }
+});
