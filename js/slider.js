f98ff90bf7e25c809cd0bf7c7bf6319f820f2cd8
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-08-09 17:27:15 -0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,51 +1,18 @@
 $(function() {
-    $('.mosaic .wraper-mosaic').slick({
-        centerMode: false,
-        slidesToShow: 5,
-        autoplay: true,
-        playspeed: 2000,
-        arrows: false,
-        infinite: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    autoplay: true,
-                    playspeed: 2000,
-                    slidesToShow: 3,
-                    infinite: true
-                }
-            },
-
-            {
-                breakpoint: 580,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    slidesToShow: 2,
-                    infinite: true
-                }
-            },
-
-            {
-                breakpoint: 380,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    slidesToShow: 1,
-                    infinite: true
-                }
-            }
-        ]
-    });
-
-    $('.statement .container').slick({
+    $('.part-2 .scroll-wrapper').slick({
         centerMode: false,
         slidesToShow: 1,
         arrows: false,
         dots: true,
-        infinite: false,
+        infinite: false
+    });
+
+    $('.call-2-slider').slick({
+        centerMode: false,
+        slidesToShow: 1,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        playspeed: 100
     });
 })
