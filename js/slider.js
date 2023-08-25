612b3f2b49efe0df7bb6d77e0088f5f3703410c3
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-08-25 19:13:49 -0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -7,12 +7,35 @@ $(function() {
         infinite: false
     });
 
-    $('.call-2-slider').slick({
-        centerMode: false,
-        slidesToShow: 1,
-        arrows: false,
-        infinite: true,
-        autoplay: true,
-        playspeed: 100
-    });
+    function runSlider(words, inputSelector) {
+        var indexWord = 0;
+        var indexChar = 0;
+    
+        var elInput = $(inputSelector);
+        elInput.focus();
+        $(window).scrollTop(0);
+    
+        run();
+    
+        function run() {
+            interval = setInterval(function() {
+                if(indexChar < words[indexWord].length) {
+                    indexChar++;
+                    elInput.val(words[indexWord].substr(0,indexChar));
+                } else if (indexWord < words.length-1) {
+                    indexWord++;
+                    indexChar = 0;
+                } else {
+                    indexWord = 0;
+                    indexChar = 0;
+                }
+            },400);
+        }
+    }
+    
+    var words1 = ['Desenvolvimento Web','Engenharia de Software','Sistemas Autênticos'];
+    runSlider(words1, '.services-slider input[type=text]');
+    
+    var words2 = ['Introdução','Nossa Equipe','Objetivos'];
+    runSlider(words2, '.about-slider input[type=text]');
 })
