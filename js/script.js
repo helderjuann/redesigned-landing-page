b3f171ef562f235368575a8c68df43655631a087
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-08-25 19:13:38 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -46,4 +46,14 @@ $(function() {
             return false;
         });
     }
+
+    disableTextS();
+
+    function disableTextS() {
+        $('body').css('-webkit-user-select','none');
+        $('body').css('-moz-user-select','none');
+        $('body').css('-ms-user-select','none');
+        $('body').css('-o-user-select','none');
+        $('body').css('user-select','none');
+    }
 });
