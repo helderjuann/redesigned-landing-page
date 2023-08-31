4c5f4588d7d12dd3ea7bd08b9a47b90961f4847b
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-08-31 18:12:10 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -56,4 +56,65 @@ $(function() {
         $('body').css('-o-user-select','none');
         $('body').css('user-select','none');
     }
+
+    validateForm();
+
+    function validateForm() {
+        $('form').submit(function() {
+
+            var e_error = false;
+            var n_name = $('[name=nome]').val();
+            var e_email = $('[name=email]').val();
+            var m_msg = $('[name=mensagem]').val();
+            resetForm();
+
+            if (n_name == "") {
+                e_error = true;
+                var nm = $('[name=nome]');
+                nm.css('border', '2px solid red');
+                $('form span').html('Por favor, insira o seu nome!');
+            } else if (e_email == "") {
+                e_error = true;
+                var em = $('[name=email]');
+                em.css('border', '2px solid red');
+                $('form span').html('Por favor, insira um e-mail válido!');
+            } else if (!e_email.includes("@") || !e_email.includes(".")) {
+                e_error = true;
+                var em = $('[name=email]');
+                em.css('border', '2px solid red');
+                $('form span').html('Por favor, insira um e-mail válido!');
+            } else if (m_msg == "") {
+                e_error = true;
+                var ms = $('[name=mensagem]');
+                ms.css('border', '2px solid red');
+                $('form span').html('Por favor, insira sua mensagem!');
+            }
+
+            if (e_error == false) {
+                $('input[type=text],textarea').fadeIn(function() {
+                    setTimeout(function() {
+                        $('input[type=text],textarea').css('border','1px solid #ccc');
+                    },2500);
+                });
+                $('input[type=text],textarea').css('border','2px solid green');
+                $('.box-success').fadeIn(function() {
+                    setTimeout(function() {
+                        $('.box-success').fadeOut();
+                    },2000);
+                });
+                $(this)[0].reset();
+            }
+
+            return false;
+        })
+    }
+
+    function resetForm() {
+        var rsForm = $('input[type=text],textarea');
+        $(rsForm).focus(function() {
+            $(this).css('border','1px solid #ccc');
+            $('form span').html('');
+        })
+    }
+
 });
